e5a6bae1ff456a37906e0de479f24f4f60cf1a42
FedeOrefici <federicoaorefici@gmail.com>
2022-04-29 02:14:30 +0200

    conflicto2

---
+++ 4.js
@@ -0,0 +1,9 @@
+let edad = 800;
+
+if(edad >= 18) {
+    console.log(`Puedes pasar, tienes ${edad} años`);
+} else {
+    console.log((`No puedes pasar, tienes ${edad} años`));
+};
+
+console.log(edad);
